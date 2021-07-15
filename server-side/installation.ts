e1962a6cc53c67a29492d8b231a66c3551b0a572
Saar Guttman <saar.g@pepperi.com>
2021-07-15 15:26:20 +0300

    Create ADAL scheme on addon installation

--- server-side/installation.ts
+++ server-side/installation.ts
@@ -9,8 +9,37 @@ The error Message is importent! it will be written in the audit log and help the
 */
 
 import { Client, Request } from '@pepperi-addons/debug-server'
+import { PapiClient } from '@pepperi-addons/papi-sdk'
 
 export async function install(client: Client, request: Request): Promise<any> {
+    const papiClient = new PapiClient({
+        token: client.OAuthAccessToken,
+        baseURL: client.BaseURL,
+        addonUUID: client.AddonUUID,
+        addonSecretKey: client.AddonSecretKey,
+        actionUUID: client['ActionUUID']
+    });
+
+    await papiClient.addons.data.schemes.post({
+        Name: "Todos",
+        Type: "indexed_data",
+        Fields:{
+            Name: {
+                Type: "String",
+                Indexed: true
+            },
+            Description: {
+                Type: "String",
+            },
+            DueDate: {
+                Type: "DateTime"
+            },
+            Completed: {
+                Type: "Bool"
+            }
+        } as any
+    });
+
     return {success:true,resultObject:{}}
 }
 
